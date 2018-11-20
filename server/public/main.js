d34eceb14fdf7d88a25a140f68f04f22fc427587
EmilSchroder <eschro66@gmail.com>
2018-11-21 11:05:46 +1300

    trash

--- server/public/main.js
+++ server/public/main.js
@@ -2,3 +2,8 @@
 //     document.getElementsByTagName('button')[0].style.height = '0'
 //     document.getElementsByTagName('button')[0].style.fontSize=0
 // }
+
+
+function balonk(){
+    document.getElementsByClassName('balloon')[0]
+}
